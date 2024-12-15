532c7ba67942a1afecf98a3020a463447da4d703
GEO-HACK <geoffreykirumba0@gmail.com>
2024-12-15 23:02:20 +0300

    blog page

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,9 +1,104 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <div className="flex flex-col gap-5 w-[80%] items-center mt-5">
+      <h1 className="text-6xl font-bold">Blogs</h1>
+      <Link className="mt-2" href="/test">
+      <div className="flex   gap-4 m-auto">
+        
+          <div className="">
+            <Image
+              src="/linker.jpg"
+              alt="the image for the page"
+              width={300}
+              height={200}
+            />
+          </div>
+          <div className="flex flex-col gap-3 w-[500px]  ">
+            <h1 className="text-4xl font-bold text-gray-300  ">Lorem ipsum dolor sit amet consectetur  </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+              deserunt, temporibus, ipsam nobis ut corrupti, vero maxime
+              recusandae beatae ducimus non? Aspernatur at sapiente amet aliquam
+              ab alias eaque. Facere.
+            </p>
+          </div>
+      </div>
+      </Link>
+      <Link className="mt-2" href="/test">
+      <div className="flex   gap-4 m-auto">
+        
+          <div className="">
+            <Image
+              src="/linker.jpg"
+              alt="the image for the page"
+              width={300}
+              height={200}
+            />
+          </div>
+          <div className="flex flex-col gap-3 w-[500px]  ">
+            <h1 className="text-4xl font-bold text-gray-300  ">Lorem ipsum dolor sit amet consectetur  </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+              deserunt, temporibus, ipsam nobis ut corrupti, vero maxime
+              recusandae beatae ducimus non? Aspernatur at sapiente amet aliquam
+              ab alias eaque. Facere.
+            </p>
+          </div>
+      </div>
+      </Link>
+      <Link className="mt-2" href="/test">
+      <div className="flex   gap-4 m-auto">
+        
+          <div className="">
+            <Image
+              src="/linker.jpg"
+              alt="the image for the page"
+              width={300}
+              height={200}
+            />
+          </div>
+          <div className="flex flex-col gap-3 w-[500px]  ">
+            <h1 className="text-4xl font-bold text-gray-300  ">Lorem ipsum dolor sit amet consectetur  </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+              deserunt, temporibus, ipsam nobis ut corrupti, vero maxime
+              recusandae beatae ducimus non? Aspernatur at sapiente amet aliquam
+              ab alias eaque. Facere.
+            </p>
+          </div>
+      </div>
+      </Link>
+      <Link className="mt-2" href="/test">
+      <div className="flex   gap-4 m-auto">
+        
+          <div className="">
+            <Image
+              src="/linker.jpg"
+              alt="the image for the page"
+              width={300}
+              height={200}
+            />
+          </div>
+          <div className="flex flex-col gap-3 w-[500px]  ">
+            <h1 className="text-4xl font-bold text-gray-300  ">Lorem ipsum dolor sit amet consectetur  </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+              deserunt, temporibus, ipsam nobis ut corrupti, vero maxime
+              recusandae beatae ducimus non? Aspernatur at sapiente amet aliquam
+              ab alias eaque. Facere.
+            </p>
+          </div>
+      </div>
+      </Link>
+      
+     
 
-export default Blog
+    </div>
+  );
+};
+
+export default Blog;
--- app/portfolio/[category]/page.jsx
+++ app/portfolio/[category]/page.jsx
@@ -1,9 +1,112 @@
-import React from 'react'
+import React from "react";
+import Button from "@/components/button/Button";
+import Image from "next/image";
 
-const Category = () => {
+const Category = async ({ params }) => {
+  const { category } = params;
+  console.log(category);
+
   return (
-    <div>Category</div>
-  )
-}
+    <div>
+      <h1 className="text-md font-bold text-green-500">{category} </h1>
+      <div className="flex flex-row gap-5 items-center">
+        <div className="flex-1 flex-col  p-4 ">
+          <h1 className="text-2xl font-bold text-gray-300 ">
+            Creative Portfolio
+          </h1>
+          <p className="text-sm ">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa
+            quaerat nesciunt aliquam illum et voluptatem non fugiat, iste ex
+            earum ea voluptate obcaecati laborum autem tenetur, quasi, porro
+            perspiciatis mollitia?
+          </p>
+          <Button text="See more" url='#' className="mt-3"/>
+        </div>
+        <div className="flex-1">
+          <div>
+            <Image
+             src="/linker.jpg"
+            alt="image of the section"
+            width={500}
+            height={500}
+            />
+          </div>
+           </div>
+      </div>
+      <div className="flex flex-row-reverse gap-5 items-center">
+        <div className="flex-1 flex-col  p-4 ">
+          <h1 className="text-2xl font-bold text-gray-300 ">
+            Creative Portfolio
+          </h1>
+          <p className="text-sm ">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa
+            quaerat nesciunt aliquam illum et voluptatem non fugiat, iste ex
+            earum ea voluptate obcaecati laborum autem tenetur, quasi, porro
+            perspiciatis mollitia?
+          </p>
+          <Button text="See more" url='#' className="mt-3"/>
+        </div>
+        <div className="flex-1">
+          <div>
+            <Image
+             src="/linker.jpg"
+            alt="image of the section"
+            width={500}
+            height={500}
+            />
+          </div>
+           </div>
+      </div>
+      <div className="flex flex-row gap-5 items-center">
+        <div className="flex-1 flex-col  p-4 ">
+          <h1 className="text-2xl font-bold text-gray-300 ">
+            Creative Portfolio
+          </h1>
+          <p className="text-sm ">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa
+            quaerat nesciunt aliquam illum et voluptatem non fugiat, iste ex
+            earum ea voluptate obcaecati laborum autem tenetur, quasi, porro
+            perspiciatis mollitia?
+          </p>
+          <Button text="See more" url='#' className="mt-3"/>
+        </div>
+        <div className="flex-1">
+          <div>
+            <Image
+             src="/linker.jpg"
+            alt="image of the section"
+            width={500}
+            height={500}
+            />
+          </div>
+           </div>
+      </div>
+      <div className="flex flex-row-reverse gap-5 items-center">
+        <div className="flex-1 flex-col  p-4 ">
+          <h1 className="text-2xl font-bold text-gray-300 ">
+            Creative Portfolio
+          </h1>
+          <p className="text-sm ">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa
+            quaerat nesciunt aliquam illum et voluptatem non fugiat, iste ex
+            earum ea voluptate obcaecati laborum autem tenetur, quasi, porro
+            perspiciatis mollitia?
+          </p>
+          <Button text="See more" url='#' className="mt-3"/>
+        </div>
+        <div className="flex-1">
+          <div>
+            <Image
+             src="/linker.jpg"
+            alt="image of the section"
+            width={500}
+            height={500}
+            />
+          </div>
+           </div>
+      </div>
+    </div>
+  );
+};
 
-export default Category
+export default Category;
